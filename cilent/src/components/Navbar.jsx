b8f3485c63be4cf681642a39ffcab3d11ef411c6
SICK <SICK@gmail.com>
2025-04-17 11:53:24 +0700

    LogoSucc

--- cilent/src/components/Navbar.jsx
+++ cilent/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Menu } from "antd";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { DownOutlined } from "@ant-design/icons";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -17,39 +18,58 @@ const Navbar = () => {
   return (
     <div
       style={{
-        width: "100%", // ✅ ให้ container กว้างเต็ม
+        width: "100%",
         backgroundColor: "#001529",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0 1rem",
       }}
     >
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <Menu
-          mode="horizontal"
-          selectedKeys={[selectedKey]}
-          onClick={(e) => navigate(e.key)}
-          theme="dark"
-          style={{
-            backgroundColor: "transparent",
-            borderBottom: "none",
-            display: "flex",
-            justifyContent: "flex-end",
-          }}
-        >
-          <Menu.Item key="/">Home</Menu.Item>
-
-          {/* SUBMENU */}
-          <SubMenu
-            key="submenu"
-            title={
-              <span>
-                {userName} <DownOutlined />
-              </span>
-            }
-          >
-            <Menu.Item key="/data">Data</Menu.Item>
-            <Menu.Item key="/login">Logout</Menu.Item>
-          </SubMenu>
-        </Menu>
+      <div>
+        <Link to="/">
+          <img
+            src={logo}
+            alt="Logo"
+            style={{
+              height: "60px",
+              maxWidth: "auto",
+              objectFit: "contain",
+              marginTop: "5px",
+              marginLeft: "1px",
+              marginBottom: "5px",
+            }}
+          />
+        </Link>
       </div>
+      <Menu
+        mode="horizontal"
+        selectedKeys={[selectedKey]}
+        onClick={(e) => navigate(e.key)}
+        theme="dark"
+        style={{
+          backgroundColor: "transparent",
+          borderBottom: "none",
+          display: "flex",
+          justifyContent: "flex-end",
+          flexGrow: 1,
+        }}
+      >
+        <Menu.Item key="/">Home</Menu.Item>
+
+        {/* SUBMENU */}
+        <SubMenu
+          key="submenu"
+          title={
+            <span>
+              {userName} <DownOutlined />
+            </span>
+          }
+        >
+          <Menu.Item key="/data">Data</Menu.Item>
+          <Menu.Item key="/login">Logout</Menu.Item>
+        </SubMenu>
+      </Menu>
     </div>
   );
 };
